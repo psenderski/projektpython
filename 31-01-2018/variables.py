8023988f8e88f4f78568e6d5be0910faeedfcbd4
Przemyslaw <psenderski@interia.pl>
2018-01-31 20:13:02 +0100

    add variables file to the repository

---
+++ 31-01-2018/variables.py
@@ -0,0 +1,22 @@
+# int
+age = 26
+# float
+temperature = 7.4
+# string
+name = 'Przemek'
+# bool
+programmer = True
+# Nonetype
+nothing = None
+print(age)
+print(type(age))
+print(temperature)
+print(name)
+#  @todo: wyświetl resztę zmiennych na ekran
+age = 52
+print(age)
+print(type(age))
+age = 'something'
+print(age)
+print(type(age))
+# @todo: wyświetl typ wszystkich zmiennych
