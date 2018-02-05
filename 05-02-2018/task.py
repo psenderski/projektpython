cb7d2b6a881221de2b6dc4f51a34b39187c657e5
Przemyslaw <psenderski@interia.pl>
2018-02-05 20:31:51 +0100

    add .gitignore

---
+++ 05-02-2018/task.py
@@ -0,0 +1,8 @@
+digit = input('Podaj liczbe: ')
+# zamiana str na int
+digit = int(digit)
+
+#@ TODO:
+if is_even:
+    print('liczba jest parzysta')
+#@ TODO: w przeciwnym wypadku wyświetl tekst "liczba nie jest parzysta'
