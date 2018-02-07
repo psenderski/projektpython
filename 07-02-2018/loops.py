7fec0262aa84c49f20fc6547dafed56766c611db
Przemyslaw <psenderski@interia.pl>
2018-02-07 18:18:10 +0100

    while loops

---
+++ 07-02-2018/loops.py
@@ -0,0 +1,11 @@
+# pętla nieskończona
+#while True:
+#   print('Hello')
+
+counter = 0
+value = input('Podaj liczbe: ')
+value = int(value)
+while counter < value:
+    print('Hello')
+    counter += 1
+print('bye')
