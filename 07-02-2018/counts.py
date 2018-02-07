a0584725cde0bddd9c596593fc44cb7be0cf05c0
Przemyslaw <psenderski@interia.pl>
2018-02-07 20:23:42 +0100

    counts

---
+++ 07-02-2018/counts.py
@@ -0,0 +1,12 @@
+# @TODO: użytkownik podaje start
+# @TODO: użytkownik podaje stop
+# @TODO: *użytkownik podaje krok
+# @TODO: wykorzystując pętle for zliczyć liczby parzyste i nieparzyste
+# @TODO: w zbiorze od START do STOP włącznie
+# @TODO: start jest zawsze większy od stop
+
+start = input('Start: ')
+stop = input('Stop: ')
+
+for idx in range(start, stop):
+    print(idx)
